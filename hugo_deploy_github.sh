2cd52adafb514a71f5e14cd95b4d97cf77d2f438
Willi Mutschler <willi@mutschler.eu>
2022-07-12 08:51:35 +0200

    Small change to deploy script

--- hugo_deploy_github.sh
+++ hugo_deploy_github.sh
@@ -2,7 +2,8 @@
 rm -rf public/*
 hugo
 cp CNAME public/CNAME
+echo "gitdir: ../.git/modules/public" > public/.git
 cd public
 git add -A
-git commit --amend -m "Built website $(date +%F)"
+git commit -m "Built website $(date +%F)"
 git push origin main --force
