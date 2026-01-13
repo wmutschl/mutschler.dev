09c1dba1d09d39892a80737dce02b26cdcbb9ff3
Willi Mutschler <willi@mutschler.eu>
2026-01-13 15:36:50 +0100

    Removed helper scripts

--- hugo_deploy_github.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf public/*
-hugo
-cp CNAME public/CNAME
-echo "gitdir: ../.git/modules/public" > public/.git
-cd public
-git add -A
-git commit --no-gpg-sign -m "Built website $(date +%F)"
-git push origin main --force
